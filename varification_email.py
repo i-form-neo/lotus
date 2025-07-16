24ad4d2367c2e2eec4f9b50c5ccfe1d42bca4a84
Valeriia Voloshyna <valeriavoloshina17@gmail.com>
2025-07-16 02:12:17 -0700

    Email_varification

---
+++ varification_email.py
@@ -0,0 +1,18 @@
+
+#валідація емейл
+
+import re
+
+def is_valid_email(email):
+    if not email:
+        return False
+    pattern = r'^[\w\.\+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z]{2,}$'
+    return re.match(pattern, email) is not None
+
+while True:
+    user_email = input("Enter your email: ").strip()
+    if is_valid_email(user_email):
+        print("✅ Email is valid, thank you!")
+        break
+    else:
+        print("❌ Email is not valid. Try again")
